c33f6bb39876754aac212af61715d09c76623359
patrickt <patrickt@rxsend.com>
2017-06-29 14:10:32 -0700

    Added more fields. View results now correctly changes results of page. By default sort by Trend, then Total Referrals column.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,6 +18,15 @@ export class DateFunc {
     dateToString: Function;
 }
 
+export class UserParams {
+    name: String;
+    zipCodes: Object;
+}
+
+export class UserFunc {
+    zipToString: Function;
+}
+
 @Component({
     moduleId: module.id,
     selector: 'home',
@@ -34,7 +43,17 @@ export class HomeComponent implements OnInit {
         totalRange: 120,
         interval: 30
     }
+    kevin: UserParams = {
+        name: "Kevin",
+        zipCodes: { 94595: "True", 94596: "True", 94598: "True", 93277: "True", 93291: "True", 93292: "True", 94587: "True", 95380: "True", 95382: "True", 95202: "True", 95203: "True", 95204: "True", 95205: "True", 95206: "True", 95207: "True", 95209: "True", 95210: "True", 95212: "True", 95215: "True", 95219: "True", 94583: "True", 94806: "True", 94401: "True", 94402: "True", 94403: "True", 94404: "True", 94497: "True", 94577: "True", 94578: "True", 94579: "True", 95110: "True", 95111: "True", 95112: "True", 95113: "True", 95116: "True", 95117: "True", 95118: "True", 95119: "True", 95120: "True", 95121: "True", 95122: "True", 95123: "True", 95124: "True", 95125: "True", 95126: "True", 95127: "True", 95128: "True", 95129: "True", 95130: "True", 95131: "True", 95132: "True", 95133: "True", 95134: "True", 95135: "True", 95136: "True", 95138: "True", 95139: "True", 95148: "True", 94102: "True", 94103: "True", 94104: "True", 94105: "True", 94107: "True", 94108: "True", 94109: "True", 94110: "True", 94111: "True", 94112: "True", 94114: "True", 94115: "True", 94116: "True", 94117: "True", 94118: "True", 94121: "True", 94122: "True", 94123: "True", 94124: "True", 94127: "True", 94128: "True", 94129: "True", 94130: "True", 94131: "True", 94132: "True", 94133: "True", 94134: "True", 94070: "True", 93901: "True", 93905: "True", 93906: "True", 93908: "True", 95814: "True", 95815: "True", 95816: "True", 95817: "True", 95818: "True", 95819: "True", 95820: "True", 95821: "True", 95822: "True", 95823: "True", 95824: "True", 95825: "True", 95826: "True", 95827: "True", 95828: "True", 95829: "True", 95830: "True", 95831: "True", 95832: "True", 95833: "True", 95834: "True", 95835: "True", 95836: "True", 95837: "True", 95838: "True", 95841: "True", 95842: "True", 95864: "True", 95661: "True", 95678: "True", 95747: "True", 95677: "True", 95765: "True", 94061: "True", 94063: "True", 94065: "True", 95630: "True", 95671: "True", 95763: "True", 94702: "True", 94703: "True", 94704: "True", 94705: "True", 94709: "True", 94710: "True", 95003: "True", 96001: "True", 96002: "True", 96003: "True", 96080: "True", 94566: "True", 94588: "True", 94301: "True", 94304: "True", 94306: "True", 95966: "True", 94601: "True", 94602: "True", 94603: "True", 94605: "True", 94606: "True", 94607: "True", 94609: "True", 94610: "True", 94612: "True", 94619: "True", 94621: "True", 94945: "True", 94947: "True", 94949: "True", 94040: "True", 94041: "True", 94043: "True", 95350: "True", 95351: "True", 95354: "True", 95355: "True", 95356: "True", 95357: "True", 95358: "True", 95348: "True", 95032: "True", 95033: "True", 94550: "True", 94541: "True", 94542: "True", 94544: "True", 94545: "True", 93230: "True", 95945: "True", 95949: "True", 95020: "True", 93701: "True", 93702: "True", 93703: "True", 93705: "True", 93710: "True", 93711: "True", 93720: "True", 93721: "True", 93722: "True", 93726: "True", 93727: "True", 93728: "True", 94536: "True", 94538: "True", 94539: "True", 94555: "True", 94518: "True", 94519: "True", 94520: "True", 94521: "True", 93301: "True", 93304: "True", 93306: "True", 93307: "True", 93308: "True", 93309: "True", 93311: "True", 93313: "True", 94509: "True", 95991: "True", 95992: "True", 95993: "True" }
+    }
 
+    userFunc: UserFunc = {
+        zipToString: function (a) {
+            a = a.charAt(0).toLowerCase() + a.slice(1);
+            return a;
+        }
+    }
     dateFunc: DateFunc = {
         dateRangeString: function (dateRanges) {
             var strArr = [];
@@ -209,6 +228,18 @@ export class HomeComponent implements OnInit {
             }
             //    console.log(doctors);
             console.log(trendArr);
+            // debugger;
+            trendArr = trendArr.sort(function (a, b) {
+                debugger;
+                if (a[3] !== "Trend") {
+                    if (a[3] < b[3]) return -1;
+                    else if (a[3] > b[3]) return 1;
+                    else if (a[4] > b[4]) return -1;
+                    else if (a[4] < b[4]) return 1;
+                    else return 0;
+                }
+            })
+            d3.select("p").selectAll("*").remove();
             var container = d3.select("p")
                 .append("table")
                 .selectAll("tr")
