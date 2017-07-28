35d5e7afab1616123ad3ace790d30d5c713bb20e
patrickt <patrickt@rxsend.com>
2017-07-28 04:19:25 -0700

    Kinda got sorting by columns dynamically working. need to fix bug of extra columns showing up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,6 +25,7 @@ export class SortOptions {
     sortTwo: String;
     sortTwoOrder: String;
     value: Object;
+    addValue: Function;
 }
 
 export class DateFunc {
@@ -74,18 +75,18 @@ export class HomeComponent implements OnInit {
 
     selections: Selections = {
         nameSelection: [
-            {id: 'Kevin', name: 'Kevin'},
-            {id: 'Rose', name: 'Rose'},
-            {id: 'RaShaud', name: 'RaShaud'},
+            { id: 'Kevin', name: 'Kevin' },
+            { id: 'Rose', name: 'Rose' },
+            { id: 'RaShaud', name: 'RaShaud' },
         ],
-        orderSelection:[
-            {id: 'Doctor', option: 'Doctor'},
-            {id: 'City', option: 'City'},
-            {id: 'Zipcode', option: 'Zipcode'},
-            {id: 'Trend', option: 'Trend'},
-            {id: 'Total Referrals', option: 'Total Referrals'},
+        orderSelection: [
+            { id: 'Doctor', option: 'Doctor' },
+            { id: 'City', option: 'City' },
+            { id: 'Zipcode', option: 'Zipcode' },
+            { id: 'Trend', option: 'Trend' },
+            { id: 'Total Referrals', option: 'Total Referrals' },
         ],
-        referralSelection:[
+        referralSelection: [
             //TODO
 
         ]
@@ -95,6 +96,14 @@ export class HomeComponent implements OnInit {
         sortOneOrder: "Ascending",
         sortTwo: "Total Referrals",
         sortTwoOrder: "Descending",
+        addValue: function (newVal, selections) {
+                //This is kind of hacked in. May need to refactor in order to improve performance.
+                debugger;
+                if (!this.value[newVal.toString()]) {
+                    selections.orderSelection.push({ id: newVal.toString(), option: newVal.toString() });
+                    this.value[newVal.toString()] = selections.orderSelection.length - 1;
+                }
+            },
         value: {
             "Doctor": 0,
             "City": 1,
@@ -221,8 +230,8 @@ export class HomeComponent implements OnInit {
 
         //Refactor code later
         var kevinZip = { 94595: "True", 94596: "True", 94598: "True", 93277: "True", 93291: "True", 93292: "True", 94587: "True", 95380: "True", 95382: "True", 95202: "True", 95203: "True", 95204: "True", 95205: "True", 95206: "True", 95207: "True", 95209: "True", 95210: "True", 95212: "True", 95215: "True", 95219: "True", 94583: "True", 94806: "True", 94401: "True", 94402: "True", 94403: "True", 94404: "True", 94497: "True", 94577: "True", 94578: "True", 94579: "True", 95110: "True", 95111: "True", 95112: "True", 95113: "True", 95116: "True", 95117: "True", 95118: "True", 95119: "True", 95120: "True", 95121: "True", 95122: "True", 95123: "True", 95124: "True", 95125: "True", 95126: "True", 95127: "True", 95128: "True", 95129: "True", 95130: "True", 95131: "True", 95132: "True", 95133: "True", 95134: "True", 95135: "True", 95136: "True", 95138: "True", 95139: "True", 95148: "True", 94102: "True", 94103: "True", 94104: "True", 94105: "True", 94107: "True", 94108: "True", 94109: "True", 94110: "True", 94111: "True", 94112: "True", 94114: "True", 94115: "True", 94116: "True", 94117: "True", 94118: "True", 94121: "True", 94122: "True", 94123: "True", 94124: "True", 94127: "True", 94128: "True", 94129: "True", 94130: "True", 94131: "True", 94132: "True", 94133: "True", 94134: "True", 94070: "True", 93901: "True", 93905: "True", 93906: "True", 93908: "True", 95814: "True", 95815: "True", 95816: "True", 95817: "True", 95818: "True", 95819: "True", 95820: "True", 95821: "True", 95822: "True", 95823: "True", 95824: "True", 95825: "True", 95826: "True", 95827: "True", 95828: "True", 95829: "True", 95830: "True", 95831: "True", 95832: "True", 95833: "True", 95834: "True", 95835: "True", 95836: "True", 95837: "True", 95838: "True", 95841: "True", 95842: "True", 95864: "True", 95661: "True", 95678: "True", 95747: "True", 95677: "True", 95765: "True", 94061: "True", 94063: "True", 94065: "True", 95630: "True", 95671: "True", 95763: "True", 94702: "True", 94703: "True", 94704: "True", 94705: "True", 94709: "True", 94710: "True", 95003: "True", 96001: "True", 96002: "True", 96003: "True", 96080: "True", 94566: "True", 94588: "True", 94301: "True", 94304: "True", 94306: "True", 95966: "True", 94601: "True", 94602: "True", 94603: "True", 94605: "True", 94606: "True", 94607: "True", 94609: "True", 94610: "True", 94612: "True", 94619: "True", 94621: "True", 94945: "True", 94947: "True", 94949: "True", 94040: "True", 94041: "True", 94043: "True", 95350: "True", 95351: "True", 95354: "True", 95355: "True", 95356: "True", 95357: "True", 95358: "True", 95348: "True", 95032: "True", 95033: "True", 94550: "True", 94541: "True", 94542: "True", 94544: "True", 94545: "True", 93230: "True", 95945: "True", 95949: "True", 95020: "True", 93701: "True", 93702: "True", 93703: "True", 93705: "True", 93710: "True", 93711: "True", 93720: "True", 93721: "True", 93722: "True", 93726: "True", 93727: "True", 93728: "True", 94536: "True", 94538: "True", 94539: "True", 94555: "True", 94518: "True", 94519: "True", 94520: "True", 94521: "True", 93301: "True", 93304: "True", 93306: "True", 93307: "True", 93308: "True", 93309: "True", 93311: "True", 93313: "True", 94509: "True", 95991: "True", 95992: "True", 95993: "True" };
-        var raShaudZip = {92506: "True", 92377: "True", 92262: "True", 92395: "True", 92307: "True", 92562: "True", 92563: "True", 92592: "True", 92543: "True", 92103: "True", 92117: "True", 92120: "True", 92123: "True", 91910: "True", 92024: "True", 92064: "True", 92663: "True", 92618: "True", 90723: "True", 91740: "True", 91723: "True", 91790: "True", 90255: "True", 91356: "True", 91360: "True", 91361: "True", 91801: "True", 91105: "True", 90505: "True", 90015: "True", 90045: "True", 90720: "True", 90712: "True", 90301: "True", 90813: "True"};
-        var roseZip = {92801: "True", 92804: "True", 91007: "True", 91760: "True", 90211: "True", 90621: "True", 90620: "True", 90703: "True", 90701: "True", 91723: "True", 90240: "True", 90241: "True", 90242: "True", 91731: "True", 91733: "True", 92840: "True", 90631: "True", 90638: "True", 90712: "True", 90808: "True", 90720: "True", 90027: "True", 90057: "True", 90020: "True", 90017: "True", 90048: "True", 90095: "True", 90015: "True", 90033: "True", 90262: "True", 90640: "True", 91754: "True", 90650: "True", 90660: "True", 91770: "True", 90404: "True", 90601: "True", 90602: "True", 90603: "True", 90605: "True", 90606: "True"};
+        var raShaudZip = { 92506: "True", 92377: "True", 92262: "True", 92395: "True", 92307: "True", 92562: "True", 92563: "True", 92592: "True", 92543: "True", 92103: "True", 92117: "True", 92120: "True", 92123: "True", 91910: "True", 92024: "True", 92064: "True", 92663: "True", 92618: "True", 90723: "True", 91740: "True", 91723: "True", 91790: "True", 90255: "True", 91356: "True", 91360: "True", 91361: "True", 91801: "True", 91105: "True", 90505: "True", 90015: "True", 90045: "True", 90720: "True", 90712: "True", 90301: "True", 90813: "True" };
+        var roseZip = { 92801: "True", 92804: "True", 91007: "True", 91760: "True", 90211: "True", 90621: "True", 90620: "True", 90703: "True", 90701: "True", 91723: "True", 90240: "True", 90241: "True", 90242: "True", 91731: "True", 91733: "True", 92840: "True", 90631: "True", 90638: "True", 90712: "True", 90808: "True", 90720: "True", 90027: "True", 90057: "True", 90020: "True", 90017: "True", 90048: "True", 90095: "True", 90015: "True", 90033: "True", 90262: "True", 90640: "True", 91754: "True", 90650: "True", 90660: "True", 91770: "True", 90404: "True", 90601: "True", 90602: "True", 90603: "True", 90605: "True", 90606: "True" };
         var userZip;
         if (this.options.name === "Kevin") {
             userZip = kevinZip;
@@ -303,8 +312,6 @@ export class HomeComponent implements OnInit {
             var newRange = getDateRanges(startDate, settings.totalRange, settings.interval, dateFunc.addDate);
             var newRangeStr = dateFunc.dateRangeString(newRange);
             for (i in parsedCSV) {
-
-
                 var drArr = parsedCSV[i];
                 var drName = drArr[0].toString();
                 var refDate = new Date(drArr[3]);
@@ -333,7 +340,6 @@ export class HomeComponent implements OnInit {
 
             //Finds trend value for each doctor-
             ///// <reference path="//" />
-
             for (i in doctors) {
                 var doctor = doctors[i];
                 var count = 0;
@@ -348,22 +354,19 @@ export class HomeComponent implements OnInit {
                     y.push(count2++);
                     x.push(parseInt(doctor[newRangeStr[i]]));
                 }
-                //   console.log("X: " + x + "||Y: " + y);
                 doctor["Total Referrals"] = count;
-
                 lr = linearRegression(y, x);
-
                 doctor["trend"] = lr["slope"];
-
             }
             var trendArr = [[]];// = [["Doctor", "City", "Zipcode", "Trend", "Total Referrals"]];
             var i;
-            for(i in selections.orderSelection){
-                debugger;
+            //Adds to the first column
+            for (i in selections.orderSelection) {
                 trendArr[0].push(selections.orderSelection[i]['option'].toString());
             }
             for (i in newRangeStr) {
-                trendArr[trendArr.length - 1].push(newRangeStr[i]);
+                trendArr[0].push(newRangeStr[i]);
+                sortOptions.addValue(newRangeStr[i], selections);
             }
             for (i in doctors) {
                 var doctor = doctors[i];
@@ -371,7 +374,7 @@ export class HomeComponent implements OnInit {
                 /**
                  * Filters out results based on trend value
                  */
-                if(filter(doctor)){
+                if (filter(doctor)) {
                     trendArr.push([i, doctor["city"], doctor["zipcode"], doctor["trend"], doctor["Total Referrals"]]);
                     for (i in newRangeStr) {
                         trendArr[trendArr.length - 1].push(doctor[newRangeStr[i]]);
